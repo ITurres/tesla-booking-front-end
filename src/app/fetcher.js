c70f6907b9a9386e7a1f64a0a758a8566482d59f
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2024-01-23 01:03:42 +0200

    Implemented global helper method for network calls

---
+++ src/app/fetcher.js
@@ -0,0 +1,21 @@
+const fetcher = async (path, method = 'GET', body = {}) => {
+  const baseUrl = process.env.REACT_APP_TESLA_API_BASE_URL;
+
+  const builtHeaders = {
+    method,
+    body: JSON.stringify(body),
+    headers: { 'content-type': 'application/json' },
+  };
+
+  try {
+    const response = await fetch(`${baseUrl}${path}`, builtHeaders);
+    if (response.ok) {
+      return await response.json();
+    }
+    throw new Error('Something went wrong...');
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export default fetcher;
