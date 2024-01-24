8b4ced510e7afbd87a9c7ddb02a1ac3448b5abb2
iturres <emanueliturres.co@gmail.com>
2024-01-24 18:01:54 -0300

    Feat: Render 'VehiclesPage' at Route index on 'App'

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,13 +3,15 @@ import { Provider } from 'react-redux';
 import store from './store';
 import Layout from '../components/Layout';
 
+import VehiclesPage from '../pages/VehiclesPage';
+
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<h1>Home</h1>} />
+            <Route index element={<VehiclesPage />} />
             <Route path="*" element={<h1>404 Not Found</h1>} />
           </Route>
         </Routes>
