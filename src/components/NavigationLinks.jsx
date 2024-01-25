ed756c9c1ae5664ae11dfb145ebb865b87a98ba7
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2024-01-26 01:52:31 +0200

    Implemented click handler with string identifier

--- src/components/NavigationLinks.jsx
+++ src/components/NavigationLinks.jsx
@@ -10,30 +10,39 @@ import '../styles/components/NavigationLinks.scss';
 const NavigationLinks = () => {
   const logged = useSelector((state) => state.users.logged);
   const dispatch = useDispatch();
+  const handleClicks = (type) => {
+    switch (type) {
+      case 'logout':
+        dispatch(userLogout());
+        break;
+      default:
+        break;
+    }
+  };
   const links = [
     {
-      id: getRandomId(), name: 'Home', url: '/', icon: <FaHouse />, level: 0, click: null,
+      id: getRandomId(), name: 'Home', url: '/', icon: <FaHouse />, level: 0, type: null,
     },
     {
-      id: getRandomId(), name: 'Reserve', url: 'reservations/new', icon: <FaCreditCard />, level: 2, click: null,
+      id: getRandomId(), name: 'Reserve', url: 'reservations/new', icon: <FaCreditCard />, level: 2, type: null,
     },
     {
-      id: getRandomId(), name: 'My Reservations', url: 'reservations', icon: <FaRegCalendar />, level: 2, click: null,
+      id: getRandomId(), name: 'My Reservations', url: 'reservations', icon: <FaRegCalendar />, level: 2, type: null,
     },
     {
-      id: getRandomId(), name: 'Insert Vehicle', url: 'vehicles/new', icon: <FaPlus />, level: 2, click: null,
+      id: getRandomId(), name: 'Insert Vehicle', url: 'vehicles/new', icon: <FaPlus />, level: 2, type: null,
     },
     {
-      id: getRandomId(), name: 'Remove Vehicle', url: 'vehicles/delete', icon: <FaTrash />, level: 2, click: null,
+      id: getRandomId(), name: 'Remove Vehicle', url: 'vehicles/delete', icon: <FaTrash />, level: 2, type: null,
     },
     {
-      id: getRandomId(), name: 'Login', url: null, icon: <FaUserLock />, level: 1, click: null,
+      id: getRandomId(), name: 'Login', url: null, icon: <FaUserLock />, level: 1, type: null,
     },
     {
-      id: getRandomId(), name: 'Register', url: null, icon: <FaUserPlus />, level: 1, click: null,
+      id: getRandomId(), name: 'Register', url: null, icon: <FaUserPlus />, level: 1, type: null,
     },
     {
-      id: getRandomId(), name: 'Logout', url: null, icon: <FaUserSlash />, level: 2, click: dispatch(userLogout()),
+      id: getRandomId(), name: 'Logout', url: null, icon: <FaUserSlash />, level: 2, type: 'logout',
     },
   ];
   return (
@@ -43,7 +52,7 @@ const NavigationLinks = () => {
           if ((!logged && link.level !== 2) || (logged && link.level !== 1)) {
             return (
               <li key={link.id}>
-                <NavLink to={link.url}>
+                <NavLink to={link.url} onClick={() => handleClicks(link.type)}>
                   {link.icon}
                   {link.name}
                 </NavLink>
