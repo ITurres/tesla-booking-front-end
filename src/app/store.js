8ee1dd4f031e96dbebe12d0b100f8c91d39ff27a
iturres <emanueliturres.co@gmail.com>
2024-01-26 21:31:38 -0300

    Feat: Update Redux store with 'reservations' reducer

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import usersReducer from '../features/users/usersSlice';
 import vehiclesListReducer from '../features/vehiclesList/vehicleListSlice';
+import reservationsReducer from '../features/reservations/reservationsSlice';
 
 export default configureStore({
   reducer: {
     users: usersReducer,
     vehiclesList: vehiclesListReducer,
+    reservationsList: reservationsReducer,
   },
 });
