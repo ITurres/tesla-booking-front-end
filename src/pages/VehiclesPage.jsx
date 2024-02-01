65bb9f22b05e515fac851a9978de98f32a053fbd
iturres <emanueliturres.co@gmail.com>
2024-02-01 14:40:43 -0300

    Feat: set page title for 'ReservationPage' and 'VehiclesPage' components

--- src/pages/VehiclesPage.jsx
+++ src/pages/VehiclesPage.jsx
@@ -3,16 +3,21 @@ import React from 'react';
 import VehiclesList from '../features/vehicles/VehiclesList';
 
 import '../styles/pages/VehiclesPage.scss';
+import setPageTitle from '../helpers/setPageTitle';
 
-const VehiclesPage = () => (
-  <section className="vehicles__section">
-    <div className="vehicles__section__overlay">
-      <h1 className="vehicles__section__title">latest models</h1>
-      <p>Please select a Tesla Model</p>
-    </div>
+const VehiclesPage = () => {
+  setPageTitle('Latest Models');
 
-    <VehiclesList />
-  </section>
-);
+  return (
+    <section className="vehicles__section">
+      <div className="vehicles__section__overlay">
+        <h1 className="vehicles__section__title">latest models</h1>
+        <p>Please select a Tesla Model</p>
+      </div>
+
+      <VehiclesList />
+    </section>
+  );
+};
 
 export default VehiclesPage;
