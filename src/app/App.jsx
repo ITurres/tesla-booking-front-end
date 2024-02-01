545dad242c343dc0b8daac9b13b68b2aca8ccc0a
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2024-02-02 00:50:13 +0200

    Imported delete vehicle list component & added its route

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -7,6 +7,7 @@ import ReservationPage from '../pages/ReservationPage';
 import AddVehiclePage from '../pages/AddVehiclePage';
 import VehicleDetail from '../features/vehicles/VehicleDetail';
 import ReservationList from '../features/reservations/ReservationsList';
+import DeleteVehicleList from '../features/vehicles/DeleteVehicleList';
 
 function App() {
   const userLogged = useSelector((state) => state.users.logged);
@@ -22,6 +23,7 @@ function App() {
               <Route path="reservations" element={<ReservationList />} />
               <Route path="reservations/new" element={<ReservationPage />} />
               <Route path="vehicles/new" element={<AddVehiclePage />} />
+              <Route path="vehicles/delete" element={<DeleteVehicleList />} />
             </>
           )}
         </Route>
