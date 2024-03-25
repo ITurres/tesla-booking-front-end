f1610f46c126253ab8145bb6159b7216738a831f
iturres <emanueliturres.co@gmail.com>
2024-03-24 23:53:15 -0300

    Feat: Implement cookie deletion on reload for user logout

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -10,6 +10,40 @@ import ReservationList from '../features/reservations/ReservationsList';
 import PageNotFound from '../components/PageNotFound';
 import DeleteVehicleList from '../features/vehicles/DeleteVehicleList';
 
+import deleteCookie from '../helpers/deleteCookie';
+import getCookie from '../helpers/getCookie';
+
+function getAndDeleteCookie() {
+  const userTokenCookieName = 'tesla-booking-user-token';
+  if (getCookie(userTokenCookieName)) {
+    deleteCookie(userTokenCookieName);
+  }
+}
+
+// * Check if the browser supports the PerformanceNavigationTiming API.
+function isNavigationTimingSupported() {
+  return 'performance' in window && 'getEntriesByType' in performance;
+}
+
+// ! When user reloads the page, the user is logged out and the token (cookie) is deleted.
+// ! Ideally, the user should stay logged in when the page is reloaded.
+// ! This is a momentary solution to delete the token when the page is reloaded, so when
+// ! the main page vehicle list is loaded, does not contain the user's vehicles.
+document.addEventListener('DOMContentLoaded', () => {
+  if (isNavigationTimingSupported()) {
+    if (performance.getEntriesByType('navigation')[0]?.type === 'reload') {
+      getAndDeleteCookie();
+    }
+  } else {
+    // eslint-disable-next-line no-lonely-if
+    if (performance.navigation.type === 1) {
+      // * Fallback ^^^ for non-supporting browsers.
+      // TODO:Note: 'navigation' is deprecated and will be removed in the future.
+      getAndDeleteCookie();
+    }
+  }
+});
+
 function App() {
   const userLogged = useSelector((state) => state.users.logged);
 
